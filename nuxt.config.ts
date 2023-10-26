543923d45c7a5b1b90d15eaaaa0de61f844a27f7
Dmitry Kungurov <125030370+yepsconnect@users.noreply.github.com>
2023-10-26 19:14:21 +0400

    feat: add yandex metrick

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss"],
+  modules: [
+    "@nuxtjs/tailwindcss",
+    [
+      "yandex-metrika-module-nuxt3",
+      {
+        id: "95378132",
+        webvisor: true,
+        // consoleLog: true,
+        // clickmap: true,
+        // useCDN: false,
+        // trackLinks: true,
+        // accurateTrackBounce: true,
+      },
+    ],
+  ],
   css: ["~/assets/css/fonts.css"],
 });
