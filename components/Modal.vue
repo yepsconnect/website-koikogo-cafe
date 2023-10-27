ea0e1c594e2cd614d88960a2bbeeb0aa2a4674aa
Dmitry Kungurov <125030370+yepsconnect@users.noreply.github.com>
2023-10-27 12:19:30 +0400

    fix: close color

--- components/Modal.vue
+++ components/Modal.vue
@@ -47,7 +47,7 @@ watch(isOpen, (newValue) => {
         >
           <path
             d="M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12L19 6.41Z"
-            fill="black"
+            class="fill-current"
             fill-opacity="0.54"
           />
         </svg>
