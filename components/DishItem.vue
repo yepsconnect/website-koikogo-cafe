4b00674329c2bec4bf30f52974c9d4a4f4da9698
Dmitry Kungurov <125030370+yepsconnect@users.noreply.github.com>
2023-10-27 13:03:36 +0400

    feat: hover

--- components/DishItem.vue
+++ components/DishItem.vue
@@ -30,34 +30,36 @@ const toggleItem = () => {
 </script>
 
 <template>
-  <div class="flex items-start gap-4">
-    <div class="avatar">
-      <div class="w-16 rounded-xl bg-gray-200">
-        <!-- <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" /> -->
-      </div>
-    </div>
-    <div class="flex flex-col md:flex-row justify-between w-full md:gap-4">
-      <div class="flex-1 flex flex-col items-start">
-        <p class="uppercase text-md">
-          {{ dish.name }}
-        </p>
-        <div
-          v-if="dish.is_kids_menu"
-          class="tooltip tooltip-right"
-          data-tip="Детское меню"
-        >
-          <button class="btn btn-sm btn-circle btn-ghost">
-            <Child class="w-7" />
-          </button>
+  <div class="flex items-center gap-1 p-2 rounded-xl hover:bg-base-200">
+    <div class="flex items-start gap-4 w-full">
+      <div class="avatar">
+        <div class="w-16 rounded-xl bg-gray-200">
+          <!-- <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" /> -->
         </div>
       </div>
+      <div class="flex flex-col md:flex-row justify-between w-full md:gap-4">
+        <div class="flex-1 flex flex-col items-start">
+          <p class="uppercase text-md">
+            {{ dish.name }}
+          </p>
+          <div
+            v-if="dish.is_kids_menu"
+            class="tooltip tooltip-right"
+            data-tip="Детское меню"
+          >
+            <button class="btn btn-sm btn-circle btn-ghost">
+              <Child class="w-7" />
+            </button>
+          </div>
+        </div>
 
-      <p class="text-md">
-        {{ dish.price }} р
-        <span class="text-sm text-gray-600">
-          {{ dish.portion_size }} {{ dish.unit }}
-        </span>
-      </p>
+        <p class="text-md">
+          {{ dish.price }} р
+          <span class="text-sm text-gray-600">
+            {{ dish.portion_size }} {{ dish.unit }}
+          </span>
+        </p>
+      </div>
     </div>
     <div class="btn btn-circle btn-xs btn-ghost">
       <svg
