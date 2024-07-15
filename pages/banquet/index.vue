42ab1c0b934fe5f85afc2cf8f8e520dd790fb919
Dmitry Kungurov <yepsconnect@icloud.com>
2024-07-15 11:27:46 +0400

    feat: add page

---
+++ pages/banquet/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <Container class="py-8 flex flex-col gap-4 items-start">
+      <h2 class="text-2xl font-bold">{{ $t('screen.banquet.title') }}</h2>
+      <p class="text-lg">{{ $t('screen.banquet.text[0]') }}</p>
+      <p class="text-lg">{{ $t('screen.banquet.text[1]') }}</p>
+      <button class="btn">{{ $t('screen.banquet.btnReserve') }}</button>
+    </Container>
+    <Container class="py-8 flex flex-col gap-4 items-start">
+      <h2 class="text-2xl font-bold">{{ $t('screen.banquet.menu.title') }}</h2>
+      <p class="text-lg">{{ $t('screen.banquet.menu.text') }}</p>
+      <button class="btn">{{ $t('screen.banquet.menu.btn') }}</button>
+    </Container>
+  </div>
+</template>
